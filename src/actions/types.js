87f31026365e21d04a381c246a63210693793e31
Dmitriy Nevzorov <jimmy.lugat@gmail.com>
2016-07-03 20:55:04 +0200

    update bundle

--- src/actions/types.js
+++
@@ -1,8 +0,0 @@
-export const SAVE_SETTINGS = 'save_settings';
-export const LOAD_SETTINGS = 'load_settings';
-export const SHOW_MESSAGE = 'show_message';
-export const CLEAN_MESSAGE = 'clean_message';
-export const FETCH_LISTS = 'fetch_lists';
-export const SEND_CAMPAIGN_REQUEST = 'send_campaign_request';
-export const SEND_CAMPAIGN_SUCCESS = 'send_campaign_success';
-export const SEND_CAMPAIGN_FAIL = 'send_campaign_fail';
